e8a7e76347d9ff53650baa39a972587ea4d58c2c
AlienegraGeek <13629402950@163.com>
2021-07-02 16:58:24 +0800

    mount

--- lib/job.go
+++ lib/job.go
@@ -48,7 +48,7 @@ func GetJob(jobName string, jobParallelism int32, deleteJobAfterFinishSec int32,
 							Name: "bee-datadir",
 							VolumeSource: corev1.VolumeSource{
 								HostPath: &corev1.HostPathVolumeSource{
-									Path: "/root/swarm/test/bee/.bee-docker",
+									Path: "/root/swarm/bee/bee-docker",
 									Type: &sectorDataDirHostType,
 								},
 							},
@@ -61,7 +61,7 @@ func GetJob(jobName string, jobParallelism int32, deleteJobAfterFinishSec int32,
 							VolumeMounts: []corev1.VolumeMount{
 								{
 									Name:      "bee-datadir",
-									MountPath: "/bee/file",
+									MountPath: "bee/file",
 								},
 							},
 							Command: []string{"/bin/sh", "-c"},
