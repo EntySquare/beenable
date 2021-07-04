9bb64f9d54c3ad3645d8630c9cd0d603e8d5cdb4
AlienegraGeek <13629402950@163.com>
2021-07-04 17:35:25 +0800

    pod && node

--- core/strategy.go
+++ core/strategy.go
@@ -10,6 +10,7 @@ import (
 	"k8s.io/apimachinery/pkg/util/rand"
 	"k8s.io/client-go/kubernetes"
 	"log"
+	"strings"
 	"time"
 )
 
@@ -30,6 +31,10 @@ type StaticStrategy struct {
 	DataDir         string
 
 	client *kubernetes.Clientset
+}
+
+type BeeKey struct {
+	beeAddr string
 }
 
 func NewStaticStrategy(swapEndpoint, swapEnable, swapGas, swapInitDeposit, debugApiEnable, networkId, mainnet, fullNode, verbosity, clefEnable, imageName, password, dataDir string) *StaticStrategy {
@@ -80,6 +85,25 @@ func (s *StaticStrategy) start() {
 	jb := lib.GetJob(jbname, 1, 10000, sf, limitList, requestList, s.SwapEndpoint, s.SwapEnable, s.SwapGas, s.SwapInitDeposit,
 		s.DebugApiEnable, s.NetworkId, s.Mainnet, s.FullNode, s.Verbosity, s.ClefEnable, s.ImageName, s.Password, s.DataDir, port1, port2, port3)
 	_, err := s.client.BatchV1().Jobs("default").Create(context.TODO(), jb, metav1.CreateOptions{})
+	pods, err := s.client.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		fmt.Print("get pod error", err)
+	}
+	var podLabelMap = make(map[string]string)
+	var nodeLabelMap = make(map[string]string)
+
+	podLabelMap["entysquare-bee-job"] = "key"
+	for _, p := range pods.Items {
+		if strings.Contains(p.Name, "entysquare-bee-job") {
+			p.SetLabels(podLabelMap)
+			node, err := s.client.CoreV1().Nodes().Get(context.TODO(), p.Spec.NodeName, metav1.GetOptions{})
+			if err != nil {
+				fmt.Print("get node error", err)
+			}
+			node.SetLabels(nodeLabelMap)
+		}
+	}
+
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -104,3 +128,23 @@ func GenerateRangeNum(min, max int) int {
 	fmt.Printf("rand is %v\n", randNum)
 	return randNum
 }
+
+//func httpGetBeeKey() *BeeKey {
+//	resp, err := http.Get("http://10.1.66.146")
+//	if err != nil {
+//		fmt.Println(err)
+//		return nil
+//	}
+//	defer resp.Body.Close()
+//	body, err := ioutil.ReadAll(resp.Body)
+//	if err != nil {
+//		fmt.Println(err)
+//		return nil
+//	}
+//	var keyData BeeKey
+//	if err := json.Unmarshal(body, &keyData); err != nil {
+//		fmt.Println(err)
+//		return nil
+//	}
+//	return keyData
+//}
