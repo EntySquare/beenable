d941e4f5945ff929fdeca966e492d168bec2a19d
AlienegraGeek <13629402950@163.com>
2021-07-08 12:03:43 +0800

    label

--- lib/job.go
+++ lib/job.go
@@ -85,7 +85,7 @@ func GetJob(jobName string, jobParallelism int32, deleteJobAfterFinishSec int32,
 								},
 							},
 							Command: []string{"/bin/bash", "-c"},
-							Args:    []string{"label"},
+							Args:    []string{"/label"},
 							//Args: []string{"label && bee start --swap-endpoint=" + swapEndpoint + " --swap-enable=" + swapEnable + " --debug-api-enable=" +
 							//	debugApiEnable + " --swap-initial-deposit=" + swapInitDeposit + " --network-id=" + networkId + " --full-node=" + fullNode +
 							//	" --verbosity=" + verbosity + " --clef-signer-enable=" + clefEnable + " --swap-deployment-gas-price " + swapGas +
