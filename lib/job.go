8d1307fe6ae97b99245a52e5cab6ac1c5e4dead1
AlienegraGeek <13629402950@163.com>
2021-07-09 14:34:46 +0800

    address

--- lib/job.go
+++ lib/job.go
@@ -75,10 +75,10 @@ func GetJob(jobName string, jobParallelism int32, deleteJobAfterFinishSec int32,
 
 							Command: []string{"/bin/bash", "-c"},
 							//Args:    []string{"/label && sleep 180s"},
-							Args: []string{"/label && sleep 120s && bee start --swap-endpoint=" + swapEndpoint + " --swap-enable=" + swapEnable + " --debug-api-enable=" +
+							Args: []string{"/label && bee start --swap-endpoint=" + swapEndpoint + " --swap-enable=" + swapEnable + " --debug-api-enable=" +
 								debugApiEnable + " --swap-initial-deposit=" + swapInitDeposit + " --network-id=" + networkId + " --full-node=" + fullNode +
 								" --verbosity=" + verbosity + " --clef-signer-enable=" + clefEnable + " --swap-deployment-gas-price " + swapGas +
-								" --password=" + password + " --data-dir=" + dataDir + "/$(cat /root/swarm/bee/.bee-docker/file/address.txt)" + " --mainnet=" + mainnet},
+								" --password=" + password + " --data-dir=" + dataDir + "/$(cat /home/bee/bee/file/address.txt)" + " --mainnet=" + mainnet},
 							Ports: []corev1.ContainerPort{
 								{
 									Name:          "api-addr",
